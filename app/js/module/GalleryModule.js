fd437cc3e8cd9b004bc47346bbc3077f5e479e87
Phonghotien <hotienphong120699@gmail.com>
2024-11-28 14:32:08 +0700

    fix-bug-28-

--- app/js/module/GalleryModule.js
+++ app/js/module/GalleryModule.js
@@ -1,14 +1,25 @@
 export default function gallery() {
-    $('.gallery').each(function() {
-        const $this = $(this);
-        const $item = $this.find('.gItem');
-        $(function() {
-            $this.lightGallery({
-                selector: $item,
-                thumbnail: true,
-                zoom: true,
-            });
-        });
+  $(".gallery").each(function () {
+    const $this = $(this);
+    const $item = $this.find(".gItem");
+    $(function () {
+      $this.lightGallery({
+        selector: $item,
+        thumbnail: true,
+        zoom: true,
+      });
     });
-
+  });
+  const galContainer = document.querySelectorAll(".gallery-container");
+  if (galContainer) {
+    galContainer.forEach((item) => {
+      const openGal = item.querySelector(".open-gallery");
+      if (openGal) {
+        openGal.addEventListener("click", () => {
+          const imgs = item.querySelectorAll(".gItem");
+          imgs[0].click();
+        });
+      }
+    });
+  }
 }
