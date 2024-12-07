9b7da5a147047db4e1dca9b2934ae868d92c5e12
ngocnhat2k1 <ngocnhat2k1@gmail.com>
2024-12-07 23:38:17 +0700

    Merge branch 'Pong_code' of https://github.com/chuanhoang11/VietNFO

--- template/js/main.js
+++ template/js/main.js
@@ -20,41 +20,43 @@ import ScrollTriggerModule from "./module/ScrollTriggerModule.js";
 import LoadMoreModule from "./module/LoadMoreModule.js";
 import ComponentModule from "./module/ComponentModule.js";
 import DatePickerModule from "./module/DatePickerModule.js";
+import FileModule from "./module/FileModule.js";
 import AdminBaseModule from "./module/AdminBaseModule.js";
 import AdminDashboardModule from "./module/AdminDashboardModule.js";
 
 window.addEventListener("DOMContentLoaded", () => {
-    // Animation
-    AnimatedModule();
-    ScrollTriggerModule();
-    AosModule();
-    // Loadmore
-    LoadMoreModule();
-    ShowHideModule();
-    // Tab
-    TabModule();
-    IsotopeModule();
-    // Upload File
-    // DateTime
-    DatePickerModule();
-    // PlusMinus
-    PlusMinusModule();
-    // Select
-    Select2Module();
-    // CountUP
-    CountUpModule();
-    // Component
-    SwiperModule();
-    MobileModule();
-    BtnToTopModule();
-    HeaderModule();
-    PopupModule();
-    CheckModule();
-    GalleryModule();
-    SearchModule();
-    SideModule();
-    CollapseModule();
-    ComponentModule();
-    AdminBaseModule();
-    AdminDashboardModule();
+  // Animation
+  AnimatedModule();
+  ScrollTriggerModule();
+  AosModule();
+  // Loadmore
+  LoadMoreModule();
+  ShowHideModule();
+  // Tab
+  TabModule();
+  IsotopeModule();
+  // Upload File
+  // DateTime
+  DatePickerModule();
+  // PlusMinus
+  PlusMinusModule();
+  // Select
+  Select2Module();
+  // CountUP
+  CountUpModule();
+  // Component
+  SwiperModule();
+  MobileModule();
+  BtnToTopModule();
+  HeaderModule();
+  PopupModule();
+  CheckModule();
+  GalleryModule();
+  SearchModule();
+  SideModule();
+  CollapseModule();
+  ComponentModule();
+  AdminBaseModule();
+  AdminDashboardModule();
+  FileModule();
 });
